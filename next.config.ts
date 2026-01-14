3050d2038d47879d36f591e941aeb765cb414e81
Kevin Nicola <kevinsamir1z32@gmail.com>
2026-01-15 01:40:50 +0200

    added : turbopack for faster devs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,//faster dev builds
+  },
 };
 
 export default nextConfig;

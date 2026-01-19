0edfa2466b3938c5fb24dd2e08b255d5c7e25b4a
Kevin Nicola <kevinsamir1z32@gmail.com>
2026-01-19 03:31:01 +0200

    added PostHog

---
+++ instrumentation-client.ts
@@ -0,0 +1,7 @@
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+});
+        
